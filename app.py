722dd4474c0c6a8d97bae96667cb91bac23070c2
Bag0niku <45112126+Bag0niku@users.noreply.github.com>
2022-08-11 20:43:55 -0700

    the flask app is working. time to start adding information

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from email import message
-from flask import Flask
 import datetime as dt
 import numpy as np
 import pandas as pd
@@ -8,9 +6,17 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+
+
+
+
+#start the Flask app
+app = Flask(__name__)
 
 ## Prepare the database
-hawaii = r"sqlite///:hawaii.sqlite"
+hawaii = r"sqlite:///hawaii.sqlite"
 engine = create_engine(hawaii)
 Base = automap_base()
 Base.prepare(engine, reflect=True)
@@ -18,26 +24,38 @@ Measurement = Base.classes.measurement
 Station = Base.classes.station
 session = Session(engine)
 
-## Start the flask app
-app = Flask(__name__)
 
-# The main page
-app.route("/")
+hello_list = ["Hello", "World!"]
+hello_dict = {"Hello": "World!"}
+
+
+@app.route("/")
 def welcome():
-    message = """ Welcome to the Climate Analysis of Ouahu, Hawaii API!
-                 Available routes:
-                 /about
-                 /precipitation
-                 /stations
-                 /temperature
-
-        
-        """
-
-# The about page
-app.route("/about")
+    return (""" 
+    <h1>Welcome to the Climate Analysis of Ouahu, Hawaii API! </h1>
+    <p>Available routes:  <ul>
+    <li>/api/v1.0/about </li>
+    <li>/api/v1.0/precipitation </li>
+    <li>/api/v1.0/stations </li>
+    <li>/api/v1.0/temperature </li></ul></p>
+    """)
 
 
-if __name__ == __main__:
+@app.route("/about")
+def spam():
+    return "Hi"
+
+@app.route("/precipitation")
+def prcp():
+    return str(hello_list)
+
+@app.route("/stations")
+def station():
+    return jsonify(hello_list)
+
+@app.route("/temperature")
+def temperature():
+    return hello_dict
+
+if __name__ == "__main__":
     app.run(debug=True)
-

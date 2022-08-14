b60d478020b4c9504d0df3fef2d83fc2fd8f2ec8
Bag0niku <45112126+Bag0niku@users.noreply.github.com>
2022-08-14 01:58:25 -0700

    Requirments are met, working on extras now

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Surfs_up
-Analysis of weather in Hawaii for a potential business venture
+Analysis of weather in Hawaii
+
+## Resources:
+- Data: hawaii.sqlite
+- Software:
+  - Python 3.7.14, Pandas 1.4.2, Numpy 1.21.5, SQLAlchemy 1.4.32, Flask 1.1.2
+    - Sometimes Windows PC's need psycopg2-binary to run SQLAlchemy
+  - Jupyter notebook (notebook server 6.4.8, Ipython 7.31.1)
+
+## Flask API
+hawaii.sqlite contains temperature and precipitation data for 9 weather stations across the island of Ouahu, Hawaii from January 1, 2010 to August 23, 2017.  I took my initial data exploration and analysis from the climate_analysis.ipynb file and turned it into an API with python's Flask framework.
+
+## Challenge
+While Most of the analysis was conducted only on the last year of available data. I did combine every month of june into it's own table and did the same with December. Comparing the two months together the first point noticed is that there is not much difference between the 2. 
+ - Junes temperatures are only slightly higher than December
+   - 25%, 50%, 75% and Maximum data points are between 2-4 degrees apart.
+   - The average is 3 degrees apart.
+ - December termperatures have a wider range with a minimum of 56 instaed of June's 64.
+ After graphing them it is easier to see that average daily temperature is about 5 degrees cooler in December than June.
--- app.py
+++ app.py
@@ -25,11 +25,6 @@ Measurement = Base.classes.measurement
 Station = Base.classes.station
 session = Session(engine)
 
-
-hello_list = ["Hello", "World!"]
-hello_dict = {"Hello": "World!"}
-
-
 @app.route("/")
 def welcome():
     return (""" 
@@ -37,13 +32,17 @@ def welcome():
     <p><b>Available routes:</b>  &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;  &nbsp; &nbsp;&nbsp; &nbsp;  &nbsp; &nbsp;&nbsp;  &nbsp; &nbsp; <b>Descriptions:</b>
     <ul>
     <li>/api/v1.0/precipitation  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;
-                            Rainfall last year.</li>
+                            Rainfall last year.
+                            </li>
     <li>/api/v1.0/stations        &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   
-                            The list of active stations.</li>    
+                            The list of active stations.
+                            </li>    
     <li>/api/v1.0/temperatures    &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  
-                            The temperatures from last year.</li>
+                            The temperatures from last year.
+                            </li>
     <li> /api/v1.0/temp/start/end   &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
-                            Find the minimum, average and maximum temeratures over a given time period from the most active station.</li>
+                            Find the minimum, average and maximum temeratures over a given time period from the most active station.
+                            </li>
     </ul></p>
     """)
 
@@ -58,6 +57,22 @@ def prcp():
     rain_last_year = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date>=last_year).all()
     rain = {date:prcp for date, prcp in rain_last_year}
     return jsonify(rain)
+
+
+@app.route("/api/v1.0/rain/<start>/<end>")
+def rain_stats(start, end):
+    select = [func.min(Measurement.prcp), func.avg(Measurement.prcp), func.max(Measurement.prcp)]
+    last_year = dt.datetime(2017, 8,23) - dt.timedelta(days=365)
+    if (start is False) and (end is False):
+        return "Dates are needed to see the temperature statistics." 
+    elif end is False:
+        results = session.query(*select).filter(Measurement.date>=start).all()
+    else: 
+        results = session.query(*select).filter(Measurement.date>=start).filter(Measurement.date<=end).all()
+    
+    rain = [{"Minimum" : r[0], "Average": r[1], "Maximum": r[2]} for r in results]
+
+
 
 @app.route("/api/v1.0/stations")
 def station():
@@ -85,7 +100,37 @@ def temp_stats(start, end):
     else: 
         results = session.query(*select).filter(Measurement.date>=start).filter(Measurement.date<=end).all()
     temps = list(np.ravel(results))
-    return jsonify(temps=temps)#, info=s)
+    temp = {"Minimum" : temps[0], "Average"] :temps[1], "Maximum": temps[2]}
+    
+
+    return jsonify(temps=temp)#, info=s)
+
+
+@app.route("/api/v1.0/stations/all_data")
+def all_data():
+    all = dict()
+    results = engine.execute(f"SELECT * FROM station;")
+    for id, station, name, lat, lon, elev in results:
+        d = dict{"name": name, "measurements": dict(), "Latitude":lat, "Longitude":lon, "elevation":elev}
+        place = engine.execute(f"SELECT * FROM measurement WHERE station = '{station};'")
+        for r in results:
+            d["measurements"][r[2]] = {"prcp": r[3] , "tobs":r[4]}
+        all[station] = d
+    
+    return jsonify(all)
+
+
+@app.route("/api/v1.0/stations/activity")
+def active_stations():
+    activity = [{"station":row[0], "activity":row[1]} for row in session.execute("SELECT COUNT(id) AS activity FROM measurement GROUP BY station ORDER BY activity DESC;")]
+    return jsonify(activity=activity)
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)

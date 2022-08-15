242b839932d8f474ba721f5712c5b08afe567cae
Bag0niku <45112126+Bag0niku@users.noreply.github.com>
2022-08-15 15:22:08 -0700

    fixed a few bugs that only occasionally showed up in the sql queries

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ from flask import Flask, jsonify
 
 
 
-
-
 #start the Flask app
 app = Flask(__name__)
 
@@ -28,30 +26,56 @@ session = Session(engine)
 @app.route("/")
 def welcome():
     return (""" 
-    <h1>Welcome to the Climate Analysis of Ouahu, Hawaii API! </h1>
-    <p><b>Available routes:</b>  &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;  &nbsp; &nbsp;&nbsp; &nbsp;  &nbsp; &nbsp;&nbsp;  &nbsp; &nbsp; <b>Descriptions:</b>
-    <ul>
-    <li>/api/v1.0/precipitation  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;
-                            Rainfall last year.
-                            </li>
-    <li>/api/v1.0/stations        &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   
-                            The list of active stations.
-                            </li>    
-    <li>/api/v1.0/temperatures    &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  
-                            The temperatures from last year.
-                            </li>
-    <li> /api/v1.0/temp/start/end   &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
-                            Find the minimum, average and maximum temeratures over a given time period from the most active station.
-                            </li>
-    </ul></p>
-    """)
+    <head>
+        <style>
+            td {padding:5px 50px 0px 0px}
+        </style>
+    </head>
+    <body>
+        <h1>Welcome to the Climate Analysis of Ouahu, Hawaii API! </h1>
+        <table>
+            <tr>
+                <th style='text-align:left'>Available routes:</th>
+                <th style='text-align:left'>Descriptions:</th>
+            </tr>
+        
+            <tr>
+                <td>/api/v1.0/rain  </td>
+                <td> Rainfall last year. </td>
+            </tr>
+            <tr>
+                <td> /api/v1.0/rain/start/end </td>
+                <td> Find the minimum, average and maximum precipitaion measurements over a given time period.</td>
+            </tr>
+            <tr>
+                <td>/api/v1.0/stations </td>
+                <td>The list of active stations.</td>
+            </tr>    
+            <tr>
+                <td>/api/v1.0/stations/activity  </td>
+                <td>The list of active stations and the number of readings within the dataset.</td>
+            </tr>    
+            <tr>
+                <td>/api/v1.0/stations/all_data    </td>
+                <td> All the data in the dataset organized by station.</td>
+            </tr>    
+            <tr>
+                <td>/api/v1.0/temperatures    </td>
+                <td>The temperatures from last year.</td>
+            </tr>
+            <tr> 
+                <td>/api/v1.0/temp/start/end </td>
+                <td>Find the minimum, average and maximum temeratures over a given time period.</td>
+            </tr>
+        </table>
+    </body>""")
 
 
 @app.route("/about")
 def spam():
     return "Hi"
 
-@app.route("/api/v1.0/precipitation")
+@app.route("/api/v1.0/rain")
 def prcp():
     last_year = dt.datetime(2017, 8, 23) - dt.timedelta(days=365)
     rain_last_year = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date>=last_year).all()
@@ -61,23 +85,21 @@ def prcp():
 
 @app.route("/api/v1.0/rain/<start>/<end>")
 def rain_stats(start, end):
-    select = [func.min(Measurement.prcp), func.avg(Measurement.prcp), func.max(Measurement.prcp)]
-    last_year = dt.datetime(2017, 8,23) - dt.timedelta(days=365)
-    if (start is False) and (end is False):
-        return "Dates are needed to see the temperature statistics." 
-    elif end is False:
-        results = session.query(*select).filter(Measurement.date>=start).all()
+    if end is False:
+        results = engine.execute(f"SELECT MIN(prcp), AVG(prcp), MAX(prcp) FROM measurement WHERE date='{start}' ;")
     else: 
-        results = session.query(*select).filter(Measurement.date>=start).filter(Measurement.date<=end).all()
+        results = engine.execute(f"SELECT MIN(prcp), AVG(prcp), MAX(prcp) FROM measurement WHERE date BETWEEN '{start}' AND '{end}' ;")
     
     rain = [{"Minimum" : r[0], "Average": r[1], "Maximum": r[2]} for r in results]
+    return jsonify(prcp=rain)
 
 
 
 @app.route("/api/v1.0/stations")
 def station():
-    results = session.query(Station.station).all()
-    stations = list(np.ravel(results))
+    results = engine.execute("SELECT * FROM station;")
+    
+    stations =[{"station":r[1], "name":r[2], "Latitude":r[3], "Longitude":r[4], "Elevation":r[5]} for r in results]
     return jsonify(stations=stations)
 
 @app.route("/api/v1.0/temperatures")
@@ -91,28 +113,23 @@ def temp():
 
 @app.route("/api/v1.0/temp/<start>/<end>")
 def temp_stats(start, end):
-    select = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
-    last_year = dt.datetime(2017, 8,23) - dt.timedelta(days=365)
-    if (start is False) and (end is False):
-        return "Dates are needed to see the temperature statistics." 
-    elif end is False:
-        results = session.query(*select).filter(Measurement.date>=start).all()
+    
+    if end is False:
+        results = engine.execute(f"SELECT MIN(tobs), AVG(tobs), MAX(tobs) FROM measurement WHERE date='{start}';")
     else: 
-        results = session.query(*select).filter(Measurement.date>=start).filter(Measurement.date<=end).all()
-    temps = list(np.ravel(results))
-    temp = {"Minimum" : temps[0], "Average" :temps[1], "Maximum": temps[2]}
+        results = engine.execute(f"SELECT MIN(tobs), AVG(tobs), MAX(tobs) FROM measurement WHERE date BETWEEN '{start}' AND '{end}';")
+    temp = [{"Minimum" : x[0], "Average" :x[1], "Maximum": x[2]} for x in results]
     
 
-    return jsonify(temps=temp)#, info=s)
-
+    return jsonify(temps=temp)
 
 @app.route("/api/v1.0/stations/all_data")
 def all_data():
     all = dict()
     results = engine.execute(f"SELECT * FROM station;")
     for id, station, name, lat, lon, elev in results:
-        d = dict{"name": name, "measurements": dict(), "Latitude":lat, "Longitude":lon, "elevation":elev}
-        for r in engine.execute(f"SELECT * FROM measurement WHERE station = '{station};'"):
+        d = {"name": name, "measurements": dict(), "Latitude":lat, "Longitude":lon, "elevation":elev}
+        for r in engine.execute(f"SELECT prcp, tobs FROM measurement WHERE station = '{station};'"):
             d["measurements"][r[2]] = {"prcp": r[3] , "tobs":r[4]}
         all[station] = d
     
@@ -121,8 +138,9 @@ def all_data():
 
 @app.route("/api/v1.0/stations/activity")
 def active_stations():
-    activity = [{"station":row[0], "activity":row[1]} for row in session.execute("SELECT COUNT(id) AS activity FROM measurement GROUP BY station ORDER BY activity DESC;")]
-    return jsonify(activity=activity)
+    results =  session.execute("SELECT station, COUNT(id) AS activity FROM measurement GROUP BY station ORDER BY activity DESC;")
+    active_count = [{"station":row[0], "activity":row[1]} for row in results]
+    return jsonify(activity=active_count)
 
 
 
